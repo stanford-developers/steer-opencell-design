84c41e669cb68b0c503b00b0ed222877752bf9ae
Nicholas Siemons <nicholas9182@gmail.com>
2025-01-07 13:42:57 -0800

    first_commit

---
+++ setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='SteerEnergyStorage',
+    version="0.0.0", 
+    description='Modelling energy storage from cell to site',
+    author='Nicholas Siemons',
+    author_email='nsiemons@stanford.edu',
+    url="https://github.com/nicholas9182/SteerEnergyStorage/",
+    packages=find_packages(),
+    install_requires=[
+        "pandas",
+        "numpy"
+    ],
+    scripts=[],
+    classifiers=[ 
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.10",
+    entry_points={
+        'console_scripts': [
+            'launch_dash_app=dash_app.app:run',
+        ],
+    }
+)
