086e19acd310431e037b95ed600a36b4856828bc
Nicholas Siemons <nicholas9182@gmail.com>
2025-01-16 16:50:38 -0800

    third_big_commit

--- SteerEnergyStorage/Formulations/Stacks.py
+++ SteerEnergyStorage/Formulations/Stacks.py
@@ -1,3 +1,4 @@
+import numpy as np
 from SteerEnergyStorage.Constructions.Electrodes import Anode
 from SteerEnergyStorage.Constructions.Electrodes import Cathode
 from SteerEnergyStorage.Materials.Seperators import Separator
@@ -8,34 +9,76 @@ class Stack():
     def __init__(self, 
                  anode: Anode,
                  cathode: Cathode,
+                 n_stacks: int,
                  seperator: Separator,
-                 anode_mass_loading: float = 1.0, 
-                 cathode_mass_loading: float = 1.0,
                  n_p_ratio: float = 1.3):
         """
         Initialize an object that represents an electrochemical stack within an electrochemical cell
-        :param anode_formulation: ElectrodeFormulation: formulation of the anode
-        :param cathode_formulation: ElectrodeFormulation: formulation of the cathode
-        :param seperator: Separator: separator used in the stack
-        :param anode_mass_loading: float: mass loading of the anode in the stack
-        :param cathode_mass_loading: float: mass loading of the cathode in the stack
-        :param n_p_ratio: float: ratio of the number of moles in the negative electrode to the number of moles in the positive electrode
+        :param anode: Anode: anode used in the stack
+        :param cathode: Cathode: cathode used in the stack
+        :param n_stacks: int: number of stacks in the cell
+        :param seperator: Separator: seperator used in the stack
+        :param n_p_ratio: float: n/p ratio of the stack
         """
+        if n_stacks != seperator.n_stacks:
+            raise ValueError("The number of stacks in the stack should be equal to the number of stacks in the seperator")
+
         self._anode = anode
         self._cathode = cathode
+        self._n_stacks = n_stacks
         self._seperator = seperator
-        self._anode_mass_loading = anode_mass_loading
-        self._cathode_mass_loading = cathode_mass_loading
         self._n_p_ratio = n_p_ratio
 
-        # get anode area
-        self._anode._single_sided_area = self._cathode._single_sided_area * (1 + self._anode._overhang / 100)
-        self._anode._coat_mass_per_sheet = self._anode.calculate_coat_mass_per_sheet()
-        self._anode._current_collector._mass = self._anode.calculate_foil_mass_per_sheet()
+        self._active_geometric_area = self._cathode._single_sided_area * 2 * self._n_stacks
 
-        # get cathode properties
-        self._cathode._coat_mass_per_sheet = self._cathode.calculate_coat_mass_per_sheet()
-        self._cathode._current_collector._mass = self._cathode.calculate_foil_mass_per_sheet()
+        # Calculate the masses in the stack
+        self._total_cathode_mass = self._cathode.mass * self._n_stacks
+        self._total_anode_mass = self._anode.mass * (self._n_stacks + 1)
+
+        # Calculate the costs in the stack
+        total_cathode_cc_cost = self._cathode._current_collector._cost * self._n_stacks
+        total_anode_cc_cost = self._anode._current_collector._cost * (self._n_stacks + 1)
+        self._total_cc_cost = total_cathode_cc_cost + total_anode_cc_cost
+
+        # calculate pore volume
+        total_cathode_pore_volume = self._cathode._pore_volume * self._n_stacks
+        total_anode_pore_volume = self._anode._pore_volume * (self._n_stacks + 1)
+        total_seperator_pore_volume = self._seperator._pore_volume
+        self._pore_volume = total_anode_pore_volume + total_cathode_pore_volume + total_seperator_pore_volume
+
+        # calculate the thickness of the stack
+        total_cathode_thickness = self._cathode._double_sided_thickness * self._n_stacks
+        total_anode_thickness = self._anode._double_sided_thickness * (self._n_stacks + 1)
+        total_seperator_thickness = self._seperator._thickness * (self._n_stacks * 2 + 3) /1000
+        self._thickness = total_anode_thickness + total_cathode_thickness + total_seperator_thickness
+
+    @property
+    def thickness(self):
+        return np.round(self._thickness, 2)
+
+    @property
+    def active_geometric_area(self):
+        return np.round(self._active_geometric_area, 2)
+
+    @property
+    def pore_volume(self):
+        return np.round(self._pore_volume, 2)
+
+    @property
+    def total_cathode_mass(self):
+        return np.round(self._total_cathode_mass, 2)
+    
+    @property
+    def total_anode_mass(self):
+        return np.round(self._total_anode_mass, 2)
+    
+    @property
+    def total_cc_cost(self):
+        return np.round(self._total_cc_cost, 2)
+
+    @property
+    def n_stacks(self):
+        return self._n_stacks
 
     @property
     def anode(self):
@@ -52,14 +95,6 @@ class Stack():
     @property
     def seperator(self):
         return self._seperator
-    
-    @property
-    def anode_mass_loading(self):
-        return self._anode_mass_loading
-    
-    @property
-    def cathode_mass_loading(self):
-        return self._cathode_mass_loading
     
     def __str__(self):
         return f"Stack with an anode formulation of {self.anode_formulation} and a cathode formulation of {self.cathode_formulation}"
--- test/test_stacks.py
+++ test/test_stacks.py
@@ -1,11 +1,15 @@
 import unittest
 from SteerEnergyStorage.Formulations.ElectrodeFormulations import ElectrodeFormulation
 from SteerEnergyStorage.Constructions.Electrodes import Cathode, Anode
+from SteerEnergyStorage.Formulations.Stacks import Stack
+from SteerEnergyStorage.Constructions.Cells import StackedPouchCell
 from SteerEnergyStorage.Materials.ElectrodeMaterials import ActiveMaterial, Binder, ConductiveAdditive
 from SteerEnergyStorage.Materials.CurrentCollectors import CurrentCollector
+from SteerEnergyStorage.Materials.Seperators import Separator
+from SteerEnergyStorage.Materials.Electrolytes import Electrolyte
 
 
-class TestElectrodes(unittest.TestCase):
+class TestCells(unittest.TestCase):
 
     def setUp(self):
         """
@@ -18,13 +22,12 @@ class TestElectrodes(unittest.TestCase):
                               density=4, 
                               irreversible_capacity_scaling=1, 
                               reversible_capacity_scaling=1)
-
+        
         cca1 = ConductiveAdditive(name="SuperC65", specific_cost=9, density=1.9)
         cb1 = Binder(name="PVDF", specific_cost=15, density=1.7)
         cf = ElectrodeFormulation(active_materials={cam1: 89}, binder={cb1: 5}, conductive_additive={cca1: 6}, calender_density=2.60)
         ccc = CurrentCollector(name="Aluminium", formula="Al", specific_cost=6.30, density=2.7, thickness=15, coated_area=172.83, bare_tab_area=8.22)
-
-        self.cathode1 = Cathode(formulation=cf, mass_loading=10.68, current_collector=ccc, swell_factor=1.0, single_sided_area=172.83)
+        cathode = Cathode(formulation=cf, mass_loading=10.68, current_collector=ccc, swell_factor=1.0, single_sided_area=172.83)
         
         # construct anode 1
         aam1 = ActiveMaterial(name="Faradion_HC", 
@@ -38,17 +41,14 @@ class TestElectrodes(unittest.TestCase):
         ab1 = Binder(name="PVDF", specific_cost=10, density=1.7)
         af = ElectrodeFormulation(active_materials={aam1: 88}, binder={ab1: 3}, conductive_additive={aca1: 9}, calender_density=0.85)
         acc = CurrentCollector(name="Aluminium", formula="Al", specific_cost=6.30, density=2.7, thickness=15, coated_area=172.83, bare_tab_area=7.55)
+        anode = Anode(formulation=af, mass_loading=5.25, current_collector=acc, swell_factor=1.0, cathode_mate_area=172.83)
 
-        self.anode1 = Anode(formulation=af, mass_loading=5.25, current_collector=acc, swell_factor=1.0, cathode_mate_area=172.83)
-
-    def test_cathode_thickness(self):
+        # construct stack
+        seperator = Separator(name="Celgard_2325", thickness=16, specific_cost=0.2, density=1.4, slit_width=100, porosity=47, fold_length=186, n_stacks=26)
+        self.stack = Stack(anode=anode, cathode=cathode, seperator=seperator, n_p_ratio=1.13, n_stacks=26)
+        
+    def test_cell_effective_areal_capacity(self):
         """
-        Test the single sided thickness of the cathode
+        Test the effective areal capacity of the cell
         """
-        self.assertEqual(self.cathode1.single_sided_thickness, 41.1)
-
-    def test_anode_thickness(self):
-        """
-        Test the single sided thickness of the anode
-        """
-        self.assertEqual(self.anode1.single_sided_thickness, 61.8)
+        self.assertEqual(type(self.stack._active_geometric_area), float)
