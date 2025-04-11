51bcab52f3b7f7d44f5702e901add4c503c6e81a
Nicholas Siemons <nicholas9182@gmail.com>
2025-04-10 17:29:43 -0700

    tweaks

--- SteerEnergyStorage/Utils.py
+++ SteerEnergyStorage/Utils.py
@@ -1,18 +1,26 @@
-import matplotlib.pyplot as plt
-import matplotlib.colors as mcolors
+import plotly.colors as pc
 import numpy as np
+
+def rgb_tuple_to_hex(rgb):
+    return '#{:02x}{:02x}{:02x}'.format(*rgb)
 
 def get_colorway(color1, color2, n):
     """
-    Function to get a colorway between two colors with n colors in between
-    
-    :param color1: color in any format supported by matplotlib
-    :param color2: color in any format supported by matplotlib
-    :param n: int: number of colors in between
-    :return: list: list of n colors in between color1 and color2
+    Generate a list of n hex colors interpolated between two HTML hex colors.
+
+    :param color1: str, hex color (e.g., '#0000ff')
+    :param color2: str, hex color (e.g., '#ffa500')
+    :param n: int, number of steps
+    :return: list of hex color strings
     """
-    rgb1 = np.array(mcolors.to_rgb(color1))
-    rgb2 = np.array(mcolors.to_rgb(color2))
-    colors = [mcolors.to_hex((1 - t) * rgb1 + t * rgb2) for t in np.linspace(0, 1, n)]
-    
+    # Convert hex to RGB (0–255)
+    rgb1 = np.array(pc.hex_to_rgb(color1))
+    rgb2 = np.array(pc.hex_to_rgb(color2))
+
+    # Interpolate and convert to hex
+    colors = [
+        rgb_tuple_to_hex(tuple(((1 - t) * rgb1 + t * rgb2).astype(int)))
+        for t in np.linspace(0, 1, n)
+    ]
+
     return colors
