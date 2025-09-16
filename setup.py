b0a50d8429b33ec59ab19c9b0ae446da2c42e660
Nicholas Siemons <nicholas9182@gmail.com>
2025-09-16 16:37:26 -0700

    form_ui

--- setup.py
+++ setup.py
@@ -16,8 +16,8 @@ setup(
     url="https://github.com/stanford-developers/steer-opencell-design/",
     packages=find_packages(),
     install_requires=[
-	    "steer-core==0.1.15",
-	    "steer-materials==0.1.9",
+	    "steer-core==0.1.16",
+	    "steer-materials==0.1.11",
         "pandas==2.1.4",
         "numpy==1.26.4",
         "datetime==5.5",
