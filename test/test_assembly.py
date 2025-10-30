c4f04369b0cc7d4acc116734c5d0436bc43bce5e
Nicholas Siemons <nicholas9182@gmail.com>
2025-10-29 17:06:42 -0700

    moved_spiral_properties_to_JellyRoll

--- test/test_assembly.py
+++ test/test_assembly.py
@@ -9,7 +9,7 @@ from steer_opencell_design.Formulations.ElectrodeFormulations import (
 from steer_opencell_design.Components.Electrodes import Cathode, Anode
 from steer_opencell_design.Components.CurrentCollectors import PunchedCurrentCollector, NotchedCurrentCollector
 from steer_opencell_design.Components.Separators import Separator
-from steer_opencell_design.Constructions.ElectrodeAssemblies import PunchedStack, ZFoldStack, WoundJellyRoll
+from steer_opencell_design.Constructions.ElectrodeAssemblies import PunchedStack, ZFoldStack, WoundJellyRoll, FlatWoundJellyRoll
 from steer_opencell_design.AuxillaryComponents.WindingEquipment import RoundMandrel, FlatMandrel
 from steer_opencell_design.Constructions.Layups import MonoLayer, ZFoldMonoLayer, Laminate
 
@@ -24,7 +24,6 @@ from steer_materials.CellMaterials.Electrode import (
     Binder,
     ConductiveAdditive,
 )
-
 
 
 class TestRoundJellyRoll(unittest.TestCase):
@@ -150,7 +149,128 @@ class TestRoundJellyRoll(unittest.TestCase):
         # fig3.show()
 
 
+class TestFlatJellyRoll(unittest.TestCase):
+    
+    def setUp(self):
+        ########################
+        # make a basic cathode
+        ########################
+        material = CathodeMaterial.from_database("LFP")
+        material.specific_cost = 6
+        material.density = 3.6
 
+        conductive_additive = ConductiveAdditive(name="super_P", specific_cost=15, density=2.0, color="#000000")
+        binder = Binder(name="CMC", specific_cost=10, density=1.5, color="#FFFFFF")
+
+        formulation = CathodeFormulation(
+            active_materials={material: 95},
+            binders={binder: 2},
+            conductive_additives={conductive_additive: 3},
+        )
+
+        current_collector_material = CurrentCollectorMaterial(name="Aluminum", specific_cost=5, density=2.7, color="#AAAAAA")
+
+        current_collector = NotchedCurrentCollector(
+            material=current_collector_material,
+            length=4500,
+            width=300,
+            thickness=8,
+            tab_width=60,
+            tab_spacing=200,
+            tab_height=18,
+            insulation_width=6,
+            coated_tab_height=2,
+        )
+
+        insulation = InsulationMaterial.from_database("Aluminium Oxide, 99.5%")
+
+        cathode = Cathode(
+            formulation=formulation,
+            mass_loading=12,
+            current_collector=current_collector,
+            calender_density=2.60,
+            insulation_material=insulation,
+            insulation_thickness=10,
+        )
+
+        material = AnodeMaterial.from_database("Synthetic Graphite")
+        material.specific_cost = 4
+        material.density = 2.2
+
+        formulation = AnodeFormulation(
+            active_materials={material: 90},
+            binders={binder: 5},
+            conductive_additives={conductive_additive: 5},
+        )
+
+        current_collector = NotchedCurrentCollector(
+            material=current_collector_material,
+            length=4500,
+            width=306,
+            thickness=8,
+            tab_width=60,
+            tab_spacing=100,
+            tab_height=18,
+            insulation_width=6,
+            coated_tab_height=2,
+        )
+
+        anode = Anode(
+            formulation=formulation,
+            mass_loading=7.2,
+            current_collector=current_collector,
+            calender_density=2.60,
+            insulation_material=insulation,
+            insulation_thickness=10,
+        )
+
+        separator_material = SeparatorMaterial(
+            name="Polyethylene",
+            specific_cost=2,
+            density=0.94,
+            color="#FDFDB7",
+            porosity=45,
+        )
+
+        top_separator = Separator(material=separator_material, thickness=25, width=310, length=5000)
+
+        bottom_separator = Separator(material=separator_material, thickness=25, width=310, length=5000)
+
+        layup = Laminate(
+            anode=anode,
+            cathode=cathode,
+            top_separator=top_separator,
+            bottom_separator=bottom_separator,
+        )
+
+        mandrel = FlatMandrel(
+            length=350,
+            width=100,
+            thickness=5
+        )
+
+        self.my_jellyroll = FlatWoundJellyRoll(
+            laminate=layup,
+            mandrel=mandrel,
+        )
+
+    def test_basics(self):
+        self.assertTrue(type(self.my_jellyroll), WoundJellyRoll)
+        self.assertAlmostEqual(self.my_jellyroll.radius, 18.01, 2)
+        self.assertAlmostEqual(self.my_jellyroll.diameter, 36.02, 2)
+        self.assertAlmostEqual(self.my_jellyroll.energy, 41.34)
+
+    def test_plots(self):
+
+        self.assertIsInstance(self.my_jellyroll, FlatWoundJellyRoll)
+        self.assertTrue(type(self.my_jellyroll.spiral) == pd.DataFrame)
+
+        fig1 = self.my_jellyroll.get_spiral_plot()
+        fig2 = self.my_jellyroll.get_spiral_plot(layered=False)
+        fig3 = self.my_jellyroll.get_capacity_plot()
+        fig1.show()
+        fig2.show()
+        # fig3.show()
 
 
 class TestPunchedStack(unittest.TestCase):
