ddba8b015b86db564ac793a4f9d3bb4c72f03097
Nicholas Siemons <nicholas9182@gmail.com>
2025-11-14 15:47:51 -0800

    updated_imports

--- steer_opencell_design/__init__.py
+++ steer_opencell_design/__init__.py
@@ -1,1 +1,26 @@
 __version__ = "0.4.6"
+
+# import current collectors
+from .Components.CurrentCollectors.Notched import NotchedCurrentCollector
+from .Components.CurrentCollectors.Tabbed import TabWeldedCurrentCollector, WeldTab
+from .Components.CurrentCollectors.Punched import PunchedCurrentCollector
+from .Components.CurrentCollectors.Tabless import TablessCurrentCollector
+
+# improt formulations
+from .Formulations.ElectrodeFormulations import AnodeFormulation, CathodeFormulation
+
+# import electrodes
+from .Components.Electrodes import Cathode, Anode
+from .Components.Separators import Separator
+
+# import layups
+from .Constructions.Layups.Laminate import Laminate
+from .Constructions.Layups.MonoLayers import ZFoldMonoLayer, MonoLayer
+
+# import electrode assemblies
+from .Constructions.ElectrodeAssemblies.Tape import Tape
+from .Constructions.ElectrodeAssemblies.JellyRolls import WoundJellyRoll, FlatWoundJellyRoll
+from .Constructions.ElectrodeAssemblies.Stacks import ZFoldStack, PunchedStack
+from .Constructions.ElectrodeAssemblies.WindingEquipment import RoundMandrel, FlatMandrel
+
+
