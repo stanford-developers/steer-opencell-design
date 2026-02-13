d052d46e6c04941fe446a7388b9c32c0927e4c17
Nicholas Siemons <nicholas9182@gmail.com>
2026-02-13 09:10:01 -0800

    vbump

--- steer_opencell_design/__init__.py
+++ steer_opencell_design/__init__.py
@@ -18,7 +18,7 @@ All public classes are re-exported from this top-level namespace for convenience
     cell = ocd.CylindricalCell(...)
 """
 
-__version__ = "1.0.18"
+__version__ = "1.0.19"
 
 # import materials
 from .Materials.ActiveMaterials import CathodeMaterial, AnodeMaterial
